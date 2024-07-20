819d1050bea4cc4e8f6b1c85f5ab07f73742cd24
AMMRISHA <ammrishachowdhury821@gmail.com>
2024-07-21 00:04:52 +0530

    second commit

--- admin/phpmailer.php
+++ admin/phpmailer.php
@@ -44,8 +44,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'rajeshsatpathi000@gmail.com';                     //SMTP username
-    $mail->Password   = 'yzwv uykc jeme iojj';                               //SMTP password
+    $mail->Username   = 'ammrishachowdhury821@gmail.com';                     //SMTP username
+    $mail->Password   = 'nccd aada wixt srdj';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $query = mysqli_query($conn, "select * from job_cetagory");
 <html lang="en">
 
 <head>
-  <title>JobPortal - Free Bootstrap 4 Template by Colorlib</title>
+  <title>JobSeeker: Online Job portal</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
